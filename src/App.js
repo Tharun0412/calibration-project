74e658f61579b4ecbf60f8f0956a17e60bbfce5d
Tharun0412 <122848239+Tharun0412@users.noreply.github.com>
2024-04-12 17:08:38 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import './App.css';
 // import Loginpage from './components/Loginpage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import Home from './components/Home'
-import Rpt1 from './components/reports/CalibrationInfoRpt'
+// import Rpt1 from './components/MeterTestRpt'
+import Rpt2 from './components/testreport/Metertestr1'
 
 function App() {
   return (
     <BrowserRouter>
             <Routes>
                {/* <Route path='/' element={<Loginpage />} /> */}
-               {/* <Route path='/' element={<Home />} /> */}
-
-                <Route path='/' element={<Rpt1 />} /> 
+               {/* <Route path='/' element={<Home />} />
+                <Route path='/Rpt1' element={<Rpt1 />} />  */}
+                <Route path='/' element={<Rpt2 />} /> 
 
            </Routes>
            </BrowserRouter>
@@ -19,3 +20,4 @@ function App() {
 }
 
 export default App;
+
